1a33e6ecdf1c3255ff050cc52b8cba1627667017
tatiSmol <judeclapton@icloud.com>
2023-10-13 23:43:12 +0300

    Added new methods to Company class.

--- src/Company.java
+++ src/Company.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class Company {
+    protected ArrayList<Employee> staffList;
+    protected int income;
+
+    public Company() {
+        staffList = new ArrayList<>();
+    }
+
+    protected void hire(Employee employee) {
+        staffList.add(employee);
+    }
+
+    protected void hireAll(Collection<Employee> employees) {
+        staffList.addAll(employees);
+    }
+
+    protected void fire(Employee employee) {
+        staffList.remove(employee);
+    }
+
+    protected int getIncome() {
+        return income;
+    }
+
+    protected List<Employee> getTopSalaryStaff(int count) {
+        return null;
+    }
+
+    protected List<Employee> getLowestSalaryStaff(int count) {
+        return null;
+    }
 }
