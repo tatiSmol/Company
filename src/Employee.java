3f3ff171f7f4bc0bf83da3f76283aaa893a17121
tatiSmol <judeclapton@icloud.com>
2023-10-13 23:56:29 +0300

    Employee class is interface now. Added new methods.

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,7 @@
-abstract class Employee {
+interface Employee extends Comparable<Employee> {
+    int getMonthSalary();
+    @Override
+    default int compareTo(Employee e) {
+        return getMonthSalary() - e.getMonthSalary();
+    }
 }
